de8279b1c7b07d99eed1cffce916b2811a6a9860
Devon Crebbin <devon.crebbin@ticteam.net>
2024-01-27 01:08:37 +1100

    updated modal styling

--- src/pages/content/index.tsx
+++ src/pages/content/index.tsx
@@ -1,48 +1,22 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import "@assets/styles/tailwind.css";
+import Modal from "@pages/content/Modal";
 import { createRoot } from "react-dom/client";
-import "./style.css";
-import React from "react";
-const div = document.createElement("div");
-div.id = "__root";
-document.body.appendChild(div);
 
-const ref = React.createRef<HTMLDivElement>();
-const inputRef = React.createRef<HTMLInputElement>();
-const offset = { x: -20, y: -80 };
+const offset = { x: -80, y: -100 };
 
-const rootContainer = document.querySelector("#__root");
-if (!rootContainer) throw new Error("Can't find Options root element");
-const root = createRoot(rootContainer);
-
-const Modal = () => {
-  return (
-    <div ref={ref} id="support" style={{ display: "none", fontSize: "10px", color: "black", background: "white", padding: "10px", position: "absolute", top: 0, zIndex: 99999 }}>
-      <h1>Show support</h1>
-      <div style={{ display: "flex" }}>
-        <input style={{ background: "white" }} ref={inputRef} value={"🖤💛❤️"}></input>
-        <button
-          onClick={() => {
-            navigator.clipboard.writeText(inputRef.current!.value);
-            ref.current!.style.display = "none";
-          }}
-        >
-          Copy
-        </button>
-      </div>
-    </div>
-  );
-};
-root.render(<Modal />);
+function showModal() {
+  const modal = document.querySelector("#support") as HTMLDivElement;
+  modal.style.display = "block";
+}
 
 function updatePosition() {
+  const modal = document.querySelector("#support") as HTMLDivElement;
   const activeElementBounds = document.activeElement?.getBoundingClientRect();
   if (!activeElementBounds) {
     return;
   }
-  ref.current!.style.transform = `translate(${activeElementBounds.x + offset.x}px, ${activeElementBounds.y + offset.y}px)`;
-}
-
-function showModal() {
-  ref.current!.style.display = "block";
+  modal.style.transform = `translate(${activeElementBounds.x + offset.x}px, ${activeElementBounds.y + offset.y}px)`;
 }
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
@@ -52,13 +26,20 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   }
 });
 
+function init() {
+  const div = document.createElement("div");
+  div.id = "__root";
+  document.body.appendChild(div);
+  const rootContainer = document.querySelector("#__root");
+  if (!rootContainer) throw new Error("Can't find Popup root element");
+  const root = createRoot(rootContainer);
+  root.render(<Modal />);
+}
+
+init();
+
 try {
   console.log("content script loaded");
 } catch (e) {
   console.error(e);
 }
-
-/*
-TODO:
-Handle the text being inserted anywhere where the cursor is
-*/
